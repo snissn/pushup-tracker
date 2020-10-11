2299f58c4cf1b7098609e94c2bf7f49f6c744e31
Mike Seiler <snissn@gmail.com>
2020-10-11 16:41:14 -0400

    pretty button

--- pages/create.js
+++ pages/create.js
@@ -4,12 +4,14 @@ import base from '../lib/db';
 import { auth } from '../lib/db';
 import * as firebase from 'firebase';
 
+import * as NumericInput from 'react-numeric-input';
+
 import { useRouter } from 'next/router';
 
 export default function Create() {
   const router = useRouter();
 
-  const { register, handleSubmit, watch, errors } = useForm();
+  const { register, handleSubmit, watch, errors, setValue } = useForm();
   const onSubmit = (data) => {
     data['userId'] = window.user.uid;
     data['createdAt'] = firebase.firestore.FieldValue.serverTimestamp();
@@ -42,9 +44,39 @@ export default function Create() {
         <div className="field">
           <label className="label">Count</label>
           <div className="control">
+            <NumericInput
+              value={1}
+              style={{
+                wrap: {
+                  boxShadow: '0 0 1px 1px #fff inset, 1px 1px 5px -1px #000',
+                  padding: '2px 2.26ex 2px 2px',
+                  borderRadius: '6px 3px 3px 6px',
+                  fontSize: 32,
+                },
+                input: {
+                  borderRadius: '4px 2px 2px 4px',
+                  padding: '0.1ex 1ex',
+                  border: '1px solid #ccc',
+                  marginRight: 4,
+                  display: 'block',
+                  fontWeight: 100,
+                  textShadow: '1px 1px 1px rgba(0, 0, 0, 0.1)',
+                },
+                'input:focus': {
+                  border: '1px inset #69C',
+                  outline: 'none',
+                },
+                arrowUp: {},
+                arrowDown: {},
+              }}
+              onChange={(val) => {
+                setValue('count', val);
+              }}
+            />
             <input
               className="input"
               name="count"
+              type="hidden"
               ref={register({ required: true })}
             />
             {/* errors will return when field validation fails  */}
