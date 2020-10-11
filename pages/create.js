3aa38c615fca654c4972dcce5cc857dc0ee16990
Mike Seiler <snissn@gmail.com>
2020-10-11 17:31:10 -0400

    more style

--- pages/create.js
+++ pages/create.js
@@ -47,16 +47,18 @@ export default function Create() {
             <NumericInput
               className="input is-fullwidth"
               value={1}
+              min={1}
               style={{
                 wrap: {
                   boxShadow: "0 0 1px 1px #fff inset, 1px 1px 5px -1px #000",
-                  padding: "2px 2.26ex 2px 2px",
+                  padding: "2px 40px 2px 2px",
                   borderRadius: "6px 3px 3px 6px",
                   fontSize: 28,
                 },
                 span: {
                   width: "100% !important",
                 },
+                plus: { width: 0 },
                 input: {
                   borderRadius: "4px 2px 2px 4px",
                   border: "1px solid #ccc",
