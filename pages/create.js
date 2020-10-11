78048b2f7fe72eebc45342cfefa616de982fd669
Mike Seiler <snissn@gmail.com>
2020-10-11 17:15:51 -0400

    button style

--- pages/create.js
+++ pages/create.js
@@ -1,25 +1,25 @@
-import React, { Component, Fragment } from 'react';
-import { useForm } from 'react-hook-form';
-import base from '../lib/db';
-import { auth } from '../lib/db';
-import * as firebase from 'firebase';
+import React, { Component, Fragment } from "react";
+import { useForm } from "react-hook-form";
+import base from "../lib/db";
+import { auth } from "../lib/db";
+import * as firebase from "firebase";
 
-import * as NumericInput from 'react-numeric-input';
+import * as NumericInput from "react-numeric-input";
 
-import { useRouter } from 'next/router';
+import { useRouter } from "next/router";
 
 export default function Create() {
   const router = useRouter();
 
   const { register, handleSubmit, watch, errors, setValue } = useForm();
   const onSubmit = (data) => {
-    data['userId'] = window.user.uid;
-    data['createdAt'] = firebase.firestore.FieldValue.serverTimestamp();
+    data["userId"] = window.user.uid;
+    data["createdAt"] = firebase.firestore.FieldValue.serverTimestamp();
 
-    console.log('data', data);
+    console.log("data", data);
 
-    base.addToCollection('pushups', data).then(function () {
-      router.push('/');
+    base.addToCollection("pushups", data).then(function () {
+      router.push("/");
     });
   };
 
@@ -49,31 +49,31 @@ export default function Create() {
               value={1}
               style={{
                 wrap: {
-                  boxShadow: '0 0 1px 1px #fff inset, 1px 1px 5px -1px #000',
-                  padding: '2px 2.26ex 2px 2px',
-                  borderRadius: '6px 3px 3px 6px',
+                  boxShadow: "0 0 1px 1px #fff inset, 1px 1px 5px -1px #000",
+                  padding: "2px 2.26ex 2px 2px",
+                  borderRadius: "6px 3px 3px 6px",
                   fontSize: 28,
                 },
                 span: {
-                  width: '100% !important',
+                  width: "100% !important",
                 },
                 input: {
-                  borderRadius: '4px 2px 2px 4px',
-                  border: '1px solid #ccc',
-                  display: 'block',
+                  borderRadius: "4px 2px 2px 4px",
+                  border: "1px solid #ccc",
+                  display: "block",
                   fontWeight: 400,
-                  textShadow: '1px 1px 1px rgba(0, 0, 0, 0.1)',
-                  'padding-left': '16px !important',
+                  textShadow: "1px 1px 1px rgba(0, 0, 0, 0.1)",
+                  "padding-left": "16px !important",
                 },
-                'input:focus': {
-                  border: '1px inset #69C',
-                  outline: 'none',
+                "input:focus": {
+                  border: "1px inset #69C",
+                  outline: "none",
                 },
                 arrowUp: {},
                 arrowDown: {},
               }}
               onChange={(val) => {
-                setValue('count', val);
+                setValue("count", val);
               }}
             />
             <input
@@ -83,28 +83,16 @@ export default function Create() {
               ref={register({ required: true })}
             />
             {/* errors will return when field validation fails  */}
-            {errors.count && <span>This field is required</span>}{' '}
+            {errors.count && <span>This field is required</span>}{" "}
           </div>
         </div>
-        <style jsx>
-          {`
-            span.react-numeric-input {
-              width: '100%';
-              paddingright: 0;
-              color: 'red';
-            }
-          `}
-        </style>
 
-        <div className="field is-grouped mt-4">
-          <div className="control">
-            <button className="button">Submit</button>
+        <div className="field is-grouped ">
+          <div className="control is-primary is-link  mt-4">
+            <button className="button is-primary">Submit</button>
           </div>
-          <div className="control">
-            <button
-              onClick={() => router.push('/')}
-              className="button is-link is-light"
-            >
+          <div className="control  mt-4">
+            <button onClick={() => router.push("/")} className="button is-link">
               Cancel
             </button>
           </div>
