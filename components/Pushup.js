92937b507d958f0601cacb504e54f14507d251eb
Mike Seiler <snissn@gmail.com>
2020-10-11 13:35:34 -0400

    color stuff

--- components/Pushup.js
+++ components/Pushup.js
@@ -1,9 +1,26 @@
 import TimeAgo from 'react-timeago';
+const stc = require('string-to-color');
+var ColorHash = require('color-hash');
 
 export default (props) => {
+  var colorHash = new ColorHash({ saturation: 0.15 });
+
   const pushup = props.pushup;
+  const thing =
+    `linear-gradient(` +
+    colorHash.hex(pushup.userId) +
+    '33,' +
+    colorHash.hex(pushup.userId) +
+    'bb)';
+  console.log('thing', thing);
   return (
-    <section className="hero is-primary is-bold  mb-6">
+    <section
+      className="hero mb-6"
+      style={{
+        color: 'blue',
+        'background-image': thing,
+      }}
+    >
       <div className="hero-body">
         <div className="container">
           <h1 className="title">
