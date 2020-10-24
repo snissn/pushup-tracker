464cccf9e8b9cec5cea11d46e88e03bfe011b56b
Mike Seiler <snissn@gmail.com>
2020-10-24 10:59:54 -0400

    add feedpage

---
+++ pages/feed.js
@@ -0,0 +1,24 @@
+import React, { Component, useState, useEffect } from "react";
+import base from "../lib/db";
+import { fetchCollectionDocs } from "../lib/utility.js";
+import PushupList from "../components/PushupList";
+
+export default (props) => {
+  const [isMounted, setMounted] = useState(false);
+  const [pushups, setPushups] = useState([]);
+
+  useEffect(() => {
+    fetchCollectionDocs("pushups").then((pushups) => {
+      if (!isMounted) {
+        setPushups(pushups);
+        setMounted(true);
+      }
+    });
+  });
+
+  return (
+    <div>
+      <PushupList pushups={pushups} />
+    </div>
+  );
+};
