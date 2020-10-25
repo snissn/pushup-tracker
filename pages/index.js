d4c69402777a9b7f18bcab15a01f98e322a9bdb2
Mike Seiler <snissn@gmail.com>
2020-10-25 12:50:20 -0400

    color

--- pages/index.js
+++ pages/index.js
@@ -28,8 +28,8 @@ export default (props) => {
           </div>
         </div>
       </section>
-      <section class="hero is-primary is-medium">
-        <div class="hero-body is-primary">
+      <section class="hero is-link is-medium">
+        <div class="hero-body ">
           <div class="container has-text-centered">
             <h1 class="title">
               Upload photos or videos of your pushups and learn exciting push up
